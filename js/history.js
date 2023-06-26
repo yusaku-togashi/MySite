f533bb002a8f80be2dd7a23730da3ceefb7361b5
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-06-26 14:44:32 +0900

    【HTML】【CSS】【JS】 仮想ページ(history)の追加

---
+++ js/history.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var items = document.querySelectorAll('.js_nav_contents');
+
+    items.forEach(function(item) {
+        var hiddenMenu = item.querySelector('.js_hidden');
+        var title = item.querySelector('.js_title');
+        if(title){
+            title.addEventListener('mouseover', function() {
+                hiddenMenu.classList.toggle('selected');
+            });
+
+            title.addEventListener('mouseout', function() {
+                hiddenMenu.classList.remove('selected');
+            });
+        }
+    });
+});
+
+$(function() {
+    $('.js_tab_button').on('click', function() {
+      $('.js_tab_button').removeClass('active');
+      $('.js_tab_item').removeClass('active');
+    
+      $(this).addClass('active');
+       
+      var index = $('.js_tab_button').index(this);
+      $('.js_tab_item').eq(index).addClass('active');
+      $('.js_tab_button').eq(index).addClass('active');
+    });
+});
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      
+      var target = $($(this).attr('href'));
+      
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 800);
+    });
+});
+  
