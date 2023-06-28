adb4bbef24046df34a83753af2bba7f8b12ca78d
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-06-28 16:10:51 +0900

    【HTML】【CSS】【JS】企業情報ページcompanyの追加、トップセクションの追加

---
+++ js/company.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var items = document.querySelectorAll('.js_nav_contents');
+
+    items.forEach(function(item) {
+        var hiddenMenu = item.querySelector('.js_hidden');
+        var title = item.querySelector('.js_title');
+        if(title || hiddenMenu){
+            title.addEventListener('mouseover', function() {
+                hiddenMenu.classList.toggle('selected');
+            });
+            hiddenMenu.addEventListener('mouseover', function() {
+                hiddenMenu.classList.toggle('selected');
+            });
+
+
+            title.addEventListener('mouseout', function() {
+                hiddenMenu.classList.remove('selected');
+            });
+            hiddenMenu.addEventListener('mouseout', function() {
+                hiddenMenu.classList.remove('selected');
+            });
+        }
+    });
+});
+
+$(function() {
+    $('.js_tab_button').on('click', function() {
+      $('.js_tab_button').removeClass('active');
+      $('.js_tab_item').removeClass('active');
+    
+      $(this).addClass('active');
+       
+      var index = $('.js_tab_button').index(this);
+      $('.js_tab_item').eq(index).addClass('active');
+      $('.js_tab_button').eq(index).addClass('active');
+    });
+});
+  
+var footBoxTitles = document.querySelectorAll('.js_box_title');
+
+footBoxTitles.forEach(function(boxTitle) {
+    boxTitle.addEventListener('click', function() {
+        var footBox = boxTitle.nextElementSibling
+        footBox.classList.toggle('active');
+        boxTitle.classList.toggle('active');
+    });
+});
