5abf929ffcd45165faa836fc08c71a7152bcfc33
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-07-05 16:55:00 +0900

    【HTML】【CSS】【JS】タブの回転動作、タブクリック後のセクションへの遷移動作の追加

--- js/sdgs.js
+++ js/sdgs.js
@@ -23,18 +23,53 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-function FixedHeader() {
-	var headerH = $('.background_image').outerHeight(true);
-	var scroll = $(window).scrollTop();
-	if (scroll >= headerH){
-			$('.header').addClass('fixed');
-            $('.navigation').addClass('fixed');
-		}else{
-			$('.header').removeClass('fixed');
-            $('.navigation').removeClass('fixed');
-		}
-}
+window.addEventListener('DOMContentLoaded', function(){
 
-$(window).scroll(function () {
-	FixedHeader();
+    $(".info").modaal({
+        overlay_close:true,
+        before_open:function(){
+            $('html').css('overflow-y','hidden');
+        },
+        after_close:function(){
+            $('html').css('overflow-y','scroll');
+        }
+    });
+});
+
+$(document).ready(function() {
+    
+    $(window).scroll(function() {
+        var isVisible = isElementVisible('.about_production_process') && isElementVisible('.about_company_environment');
+
+        if (isVisible) {
+            $('.about_production_process').addClass('box_rotateX');
+            $('.about_company_environment').addClass('box_rotateX2');
+        }
+    });
+
+
+    function isElementVisible(selector) {
+        var $element = $(selector);
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var elementTop = $element.offset().top;
+        var elementBottom = elementTop + $element.outerHeight();
+
+        
+        var isVisible = (elementTop + 150 <= (scrollTop + windowHeight)) && (elementBottom >= scrollTop);
+    
+        return isVisible;
+    }
+});
+
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      
+      var target = $($(this).attr('href'));
+      
+      $('html, body').animate({
+        scrollTop: target.offset().top -100
+      }, 500);
+    });
 });
