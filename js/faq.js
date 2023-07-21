fb9e0c3eeeabb63da5b4314398a75b3c601856f7
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-07-21 18:06:43 +0900

    【CSS】【JS】カテゴリーで選ぶセクションのJS動作の修正、それに伴ったCSSの修正

--- js/faq.js
+++ js/faq.js
@@ -22,18 +22,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-    function isElementVisible(selector) {
-        var $element = $(selector);
-        var scrollTop = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        var elementTop = $element.offset().top;
-        var elementBottom = elementTop + $element.outerHeight();
 
-        
-        var isVisible = (elementTop + 150 <= (scrollTop + windowHeight)) && (elementBottom >= scrollTop);
+function isElementVisible(selector) {
+    var $element = $(selector);
+    var scrollTop = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var elementTop = $element.offset().top;
+    var elementBottom = elementTop + $element.outerHeight();
+
     
-        return isVisible;
-    }
+    var isVisible = (elementTop + 150 <= (scrollTop + windowHeight)) && (elementBottom >= scrollTop);
+
+    return isVisible;
+}
 
 var footBoxTitles = document.querySelectorAll('.js_box_title');
 
@@ -45,13 +46,48 @@ footBoxTitles.forEach(function(boxTitle) {
     });
 });
 
+// サーチタブ
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(event) {
+      event.preventDefault();
+      
+      var target = $($(this).attr('href'));
+      
+      $('html, body').animate({
+        scrollTop: target.offset().top -70
+      }, 500);
+    });
+});
 
-
+// カテゴリーで調べる
 const accordionTitle = document.querySelectorAll('.jsAccordionTitle');
+const categoryTitle = document.querySelectorAll('.jsCategoryTitle')
+const openCategoryTitle = document.querySelectorAll('.js_faq_accordion_content');
 
+console.log(categoryTitle);
+
+for (let i = 0; i < categoryTitle.length; i++){
+    categoryTitle[i].addEventListener('click', function() {
+      openCategoryTitle[i].classList.add('is_open');
+      accordionTitle[i].classList.add('active');
+    });
+}
+
+// 一覧で見る
 for (let i = 0; i < accordionTitle.length; i++){
   accordionTitle[i].addEventListener('click', function() {
-    this.classList.toggle('is-active');
+    this.classList.toggle('active')
     this.nextElementSibling.classList.toggle('is_open');
   });
 }
+
+const boxTitle = document.querySelectorAll('.jsBoxTitle');
+
+for (let i = 0; i < boxTitle.length; i++){
+    boxTitle[i].addEventListener('click', function() {
+      this.classList.toggle('active');
+      this.classList.toggle('is_open');
+      this.nextElementSibling.classList.toggle('is_open');
+
+    });
+  }
