3daa9f7f4d0b82686cd38c6c6f9e8662a24b9eef
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-07-21 18:37:18 +0900

    【HTML】【CSS】【JS】スクロール時のフェードインアニメーションの実装

--- js/javascript.js
+++ js/javascript.js
@@ -47,14 +47,17 @@ $(window).scroll(function () {
 	FixedHeader();
 });
 
-function fadeinBlock() {
-	var headerH = $('.products').outerHeight(true);
-	var scroll = $(window).scrollTop();
+$(window).scroll(function (){
+	$('.js_fadein_block').each(function(){
+		var elemPos = $(this).offset().top,
+		scroll = $(window).scrollTop(),
+		windowHeight = $(window).height();
 
-	if (scroll >= 1500){
-			$('.products').addClass('fadein');
-		}
-}
+			if (scroll > elemPos - windowHeight + 150){
+				$(this).addClass('fadein');
+			}
+	});
+});
 
 $(window).scroll(function () {
 	fadeinBlock();
