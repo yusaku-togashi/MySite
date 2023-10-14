087c6ffa6ecdd21f29133e20ffb51df63e878d83
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-10-15 00:23:51 +0900

    feat : 新規ファイルの追加

---
+++ js/united_information.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+  fade:true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed:1000,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+});
