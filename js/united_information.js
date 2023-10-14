d3556d66f91a68cbd07210d301cb70b310b157f3
togashi yuusaku <togashi.y@tsunagu-grp.jp>
2023-10-15 03:09:12 +0900

    feat : wip 画像、デザインの追加

--- js/united_information.js
+++ js/united_information.js
@@ -1,4 +1,4 @@
-$('.slider').slick({
+$('.slider-mv').slick({
   fade:true,
   autoplay: true,
   autoplaySpeed: 3000,
@@ -8,4 +8,46 @@ $('.slider').slick({
   slidesToScroll: 1,
   arrows: false,
   dots: false,
+});
+
+function GethashID(hashIDName) {
+  if (hashIDName) {
+    $('.tab li').find('a').each(function(){
+      var idName = $(this).attr('href');
+      if(idName == hashIDName){
+        var parentElm = $(this).parent();
+        $('.tab li').removeClass("active");
+        $(parentElm).addClass("active");
+        $(".area").removeClass("is-active");
+        $(hashIDName).addClass("is-active");
+      }
+    });
+  }
+}
+
+$('.tab a').on('click', function(){
+  var idName = $(this).attr('href');
+  GethashID(idName);
+  console.log(idName)
+  return false;
+});
+
+$(window).on('load', function(){
+  $('.tab li:first-of-type').addClass("active");
+  $('.area:first-of-type').addClass("is-active");
+  var hashName = location.hash;
+  GethashID(hashName);
+})
+
+$('.slider-result').slick({
+  autoplay: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<div class="slick-prev"></div>',
+  nextArrow: '<div class="slick-next"></div>',
+  centerMode: true,
+  variableWidth: true,
+  dots: true,
 });
